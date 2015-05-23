0fa3d80a606f9d706b95f98398e94020c02648a1
Mátyás Mustoha <matyas.mustoha@gmail.com>
2015-05-23 15:34:54 +0200

    Added ARM_OVERRIDE_ARCH build flag.

--- cmake/PlatformArm.cmake
+++ cmake/PlatformArm.cmake
@@ -3,12 +3,18 @@
 
 # If you get compiling or linking errors, try editing this file
 
+if (ARM_OVERRIDE_ARCH)
+	set(ARM_TARGET_ARCH "${ARM_OVERRIDE_ARCH}")
+else()
+	set(ARM_TARGET_ARCH "${CMAKE_SYSTEM_PROCESSOR}")
+endif()
+
 set(ARM_FLAGS "-marm")
-if (CMAKE_SYSTEM_PROCESSOR MATCHES "^armv6")
+if (ARM_TARGET_ARCH MATCHES "^armv6")
 	set(ARM_FLAGS "${ARM_FLAGS} -march=armv6 -mfpu=vfp -mfloat-abi=hard")
-elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "^armv7")
+elseif (ARM_TARGET_ARCH MATCHES "^armv7")
 	set(ARM_FLAGS "${ARM_FLAGS} -march=armv7-a -mfpu=neon -mfloat-abi=softfp")
 endif()
 
-SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${ARM_FLAGS}")
-SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${ARM_FLAGS}")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${ARM_FLAGS}")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${ARM_FLAGS}")
