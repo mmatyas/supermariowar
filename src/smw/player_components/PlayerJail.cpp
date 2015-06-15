01d0a3393b05c599dc9624530e562a2036ec4c05
Mátyás Mustoha <matyas.mustoha@gmail.com>
2015-06-15 20:59:09 +0200

    Jail part 2.

---
+++ src/smw/player_components/PlayerJail.cpp
@@ -0,0 +1,107 @@
+#include "PlayerJail.h"
+
+#include "eyecandy.h"
+#include "player.h"
+#include "GameValues.h"
+#include "ResourceManager.h"
+
+#include <cassert>
+
+extern CEyecandyContainer eyecandy[3];
+extern CGameValues game_values;
+extern CResourceManager* rm;
+
+PlayerJail::PlayerJail()
+    : owner_teamID(-1)
+    , color(0)
+    , timer(0)
+{}
+
+bool PlayerJail::isActive()
+{
+    return timer > 0;
+}
+
+void PlayerJail::setColor(int8_t color)
+{
+    assert(color < 4 && color >= -1);
+    this->color = color;
+}
+
+int8_t PlayerJail::getColor()
+{
+    return color;
+}
+
+void PlayerJail::lockInBy(CPlayer& inflictor)
+{
+    timer = game_values.gamemodesettings.jail.timetofree;
+    owner_teamID = inflictor.teamID;
+
+    if (game_values.gamemodesettings.jail.style == 1)
+        color = inflictor.colorID;
+    else
+        color = -1;
+}
+
+void PlayerJail::escape(CPlayer& player)
+{
+    if (isActive()) {
+        timer = 0;
+        owner_teamID = -1;
+
+        eyecandy[2].add(new EC_SingleAnimation(&rm->spr_poof,
+            player.ix + HALFPW - 24,
+            player.iy + HALFPH - 24,
+            4, 5));
+        ifSoundOnPlay(rm->sfx_transform);
+    } else {
+        ifSoundOnPlay(rm->sfx_hit);
+    }
+}
+
+void PlayerJail::free_by_teammate(CPlayer& player)
+{
+    if (isActive()) {
+        timer = 0;
+        eyecandy[2].add(new EC_SingleAnimation(&rm->spr_fireballexplosion,
+            player.ix + HALFPW - 16,
+            player.iy + HALFPH - 16,
+            3, 8));
+        ifSoundOnPlay(rm->sfx_transform);
+    }
+}
+
+void PlayerJail::update(CPlayer& player)
+{
+    if (isActive() && game_values.gamemodesettings.jail.timetofree > 1) {
+        if (--timer <= 0) {
+            timer = 0;
+            owner_teamID = -1;
+            eyecandy[2].add(new EC_SingleAnimation(&rm->spr_fireballexplosion,
+                player.ix + HALFPW - 16,
+                player.iy + HALFPH - 16,
+                3, 8));
+            ifSoundOnPlay(rm->sfx_transform);
+        }
+    }
+}
+
+void PlayerJail::draw(CPlayer& player)
+{
+    if (isActive()) {
+        if (player.iswarping())
+            rm->spr_jail.draw(
+                player.ix - PWOFFSET - 6,
+                player.iy - PHOFFSET - 6,
+                (color + 1) * 44,
+                0, 44, 44,
+                (short)player.state % 4, player.GetWarpPlane());
+        else
+            rm->spr_jail.draw(
+                player.ix - PWOFFSET - 6,
+                player.iy - PHOFFSET - 6,
+                (color + 1) * 44,
+                0, 44, 44);
+    }
+}
---
+++ src/smw/player_components/PlayerJail.h
@@ -0,0 +1,33 @@
+#ifndef PLAYER_JAIL_STATUS
+#define PLAYER_JAIL_STATUS
+
+#include <stdint.h>
+
+class CPlayer;
+
+class PlayerJail
+{
+public:
+    PlayerJail();
+    void update(CPlayer& player);
+    void draw(CPlayer& player);
+
+    bool isActive();
+
+    int8_t getColor();
+    void setColor(int8_t color);
+
+    void escape(CPlayer& player);
+    void free_by_teammate(CPlayer& player);
+    void lockInBy(CPlayer& inflictor);
+
+private:
+    int8_t owner_teamID;
+    int8_t color;
+    short timer;
+
+friend class CGM_Jail;
+};
+
+
+#endif // PLAYER_JAIL_STATUS
