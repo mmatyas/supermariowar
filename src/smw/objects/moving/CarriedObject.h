16fd9ce0a5917f486f0d90ca88a64c414f184750
Mátyás Mustoha <matyas.mustoha@gmail.com>
2015-06-15 20:22:56 +0200

    WIP: MO_CarriedObject done.

--- src/smw/objects/moving/CarriedObject.h
+++ src/smw/objects/moving/CarriedObject.h
@@ -1,47 +1,45 @@
-#ifndef SMW_GAMEOBJECT_BLOCK_MO_CarriedObject_H
-#define SMW_GAMEOBJECT_BLOCK_MO_CarriedObject_H
+#ifndef SMW_GAMEOBJECT_BLOCK_MO_CARRIEDOBJECT_H
+#define SMW_GAMEOBJECT_BLOCK_MO_CARRIEDOBJECT_H
+
+#include "MovingObject.h"
+
+class gfxSprite;
+class CPlayer;
 
 class MO_CarriedObject : public IO_MovingObject
 {
-	public:
-		MO_CarriedObject(gfxSprite *nspr, short x, short y, short iNumSpr, short aniSpeed, short iCollisionWidth, short iCollisionHeight, short iCollisionOffsetX, short iCollisionOffsetY, short iAnimationOffsetX, short iAnimationOffsetY, short iAnimationHeight, short iAnimationWidth);
-		MO_CarriedObject(gfxSprite *nspr, short x, short y, short iNumSpr, short aniSpeed, short iCollisionWidth, short iCollisionHeight, short iCollisionOffsetX, short iCollisionOffsetY);
-		~MO_CarriedObject();
+public:
+    MO_CarriedObject(gfxSprite *nspr, short x, short y, short iNumSpr, short aniSpeed, short iCollisionWidth, short iCollisionHeight, short iCollisionOffsetX, short iCollisionOffsetY, short iAnimationOffsetX, short iAnimationOffsetY, short iAnimationHeight, short iAnimationWidth);
+    MO_CarriedObject(gfxSprite *nspr, short x, short y, short iNumSpr, short aniSpeed, short iCollisionWidth, short iCollisionHeight, short iCollisionOffsetX, short iCollisionOffsetY);
+    ~MO_CarriedObject();
 
-		virtual void update() {}
-		virtual void draw();
-    virtual bool collide(CPlayer *) {
-        return false;
-    }
+    virtual void update() {}
+    virtual void draw();
+    virtual bool collide(CPlayer *) { return false; }
 
-		virtual void MoveToOwner();
+    virtual void MoveToOwner();
 
-		virtual void Drop();
-		virtual void Kick();
+    virtual void Drop();
+    virtual void Kick();
 
-    bool HasOwner() {
-        return owner != NULL;
-    }
+    bool HasOwner() { return owner != NULL; }
+    bool IsCarriedByKuriboShoe() { return fCarriedByKuriboShoe; }
 
-    bool IsCarriedByKuriboShoe() {
-        return fCarriedByKuriboShoe;
-    }
+protected:
 
-	protected:
+    bool fCarriedByKuriboShoe;
 
-		bool fCarriedByKuriboShoe;
+    CPlayer * owner;
+    bool fSmoking;
 
-		CPlayer * owner;
-		bool fSmoking;
+    float dKickX, dKickY;
+    short iOwnerLeftOffset, iOwnerRightOffset, iOwnerUpOffset;
 
-		float dKickX, dKickY;
-		short iOwnerLeftOffset, iOwnerRightOffset, iOwnerUpOffset;
+private:
+    void init();
 
-	private:
-		void init();
-
-	friend class B_ThrowBlock;
-	friend class CPlayer;
+friend class B_ThrowBlock;
+friend class CPlayer;
 };
 
-#endif // SMW_GAMEOBJECT_BLOCK_MO_CarriedObject_H
+#endif // SMW_GAMEOBJECT_BLOCK_MO_CARRIEDOBJECT_H
