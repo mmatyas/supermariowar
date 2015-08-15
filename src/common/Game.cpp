61459ce81afc6d36ad80d6733d56ab0a14f8a1ae
Mátyás Mustoha <matyas.mustoha@gmail.com>
2015-08-15 23:13:50 +0200

    Added settings directory creation fo Unix systems.

--- src/common/Game.cpp
+++ src/common/Game.cpp
@@ -6,6 +6,12 @@
 #include <windows.h>
 #endif
 
+#if defined(unix) || defined(__unix__) || defined(__unix)
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#endif
+
 CGame::CGame(const char *rootDirectory)
 {
     // make sure that the .smw directory is created
@@ -20,7 +26,13 @@ CGame::CGame(const char *rootDirectory)
     	//TODO: print that we created the directory
     }
 #else
-    //TODO: create directory in MacOSX/Linux systems
+    struct stat st;
+    if (stat(smwHome.c_str(), &st) != 0) { // inode does not exist
+        if (mkdir(smwHome.c_str(), 0775) != 0)
+            perror("[error] Could not create settings directory");
+    }
+    else if (!S_ISDIR(st.st_mode)) // inode exist, but not a directory
+        perror("[error] Could not access settings directory");
 #endif
 
 #ifdef _XBOX
