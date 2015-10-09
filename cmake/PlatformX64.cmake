9f33e403e0db132b0ba40330d9ab419673721867
Mátyás Mustoha <matyas.mustoha@gmail.com>
2015-10-09 20:31:41 +0200

    Use SSE build flags only on x86 and amd64.

---
+++ cmake/PlatformX64.cmake
@@ -0,0 +1,5 @@
+# AMD64/x86_64 specific compiler settings
+# ---------------------------------------
+
+# The same as for x86
+include(PlatformX86)
---
+++ cmake/PlatformX86.cmake
@@ -0,0 +1,5 @@
+# x86 specific compiler settings
+# ------------------------------
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -msse2")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -msse2")
