28e644a61a3ce76c8f2e6b5fc4022b0121531cd0
Mátyás Mustoha <matyas.mustoha@gmail.com>
2015-12-25 21:42:16 +0100

    Rewrite File IO to an interface above file operations

--- src/common/FileIO.h
+++ src/common/FileIO.h
@@ -1,25 +1,44 @@
 #ifndef FILEIO_H
 #define FILEIO_H
 
-#include "SDL.h"
-#include <stdio.h>
+#include <stdio.h> // size_t
+#include <stdint.h>
 
-void fread_or_exception(void* ptr, size_t size, size_t count, FILE* stream);
-void fwrite_or_exception(const void* ptr, size_t size, size_t count, FILE* stream);
+class BinaryFile {
+public:
+    BinaryFile(const char* filename, const char* options);
+    ~BinaryFile();
 
-FILE* OpenFile(const char * filename, const char * options);
-void WriteInt(int out, FILE * outFile);
-int ReadInt(FILE * inFile);
-void ReadIntChunk(int * mem, size_t iQuantity, FILE * inFile);
-void WriteFloat(float out, FILE * outFile);
-float ReadFloat(FILE * inFile);
-void WriteString(const char * szString, FILE * outFile);
-void ReadString(char * szString, short size, FILE * outFile);
-void WriteByteFromShort(short out, FILE * outFile);
-short ReadByteAsShort(FILE * inFile);
-void WriteBool(bool out, FILE * outFile);
-bool ReadBool(FILE * inFile);
-void WriteByte(Uint8 out, FILE * outFile);
-Uint8 ReadByte(FILE * inFile);
+    bool is_open();
+    void rewind();
+
+    void write_i8(int8_t);
+    void write_u8(uint8_t);
+    void write_i16(int16_t);
+    void write_i32(int32_t);
+    void write_bool(bool);
+    void write_float(float);
+    void write_string(const char*);
+    void write_string_long(const char*);
+    void write_raw(void*, size_t);
+
+    int8_t read_i8();
+    uint8_t read_u8();
+    int16_t read_i16();
+    int32_t read_i32();
+    bool read_bool();
+    float read_float();
+    void read_i16_array(int16_t*, size_t);
+    void read_i32_array(int32_t*, size_t);
+    void read_string(char*, size_t);
+    void read_string_long(char*, size_t);
+    void read_raw(void*, size_t);
+
+private:
+    FILE* fp;
+
+    void fread_or_exception(void*, size_t, size_t);
+    void fwrite_or_exception(const void*, size_t, size_t);
+};
 
 #endif // FILEIO_H
