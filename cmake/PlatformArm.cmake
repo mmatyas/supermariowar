05f7ba7ca2bd74c4fb8312b139a46c5ac1b34111
Mátyás Mustoha <matyas.mustoha@gmail.com>
2016-02-05 20:34:03 +0100

    Added DISABLE_DEFAULT_CFLAGS option, and removed ARM_OVERRIDE_ conditions

--- cmake/PlatformArm.cmake
+++ cmake/PlatformArm.cmake
@@ -1,28 +1,18 @@
 # ARM specific compiler settings
 # ------------------------------
 
-# Overrides CMAKE_SYSTEM_PROCESSOR if set
-if (ARM_OVERRIDE_ARCH)
-	set(ARM_TARGET_ARCH "${ARM_OVERRIDE_ARCH}")
+set(ARM_FLAGS "-marm")
+if (CMAKE_SYSTEM_PROCESSOR MATCHES "^armv6")
+	set(ARM_FLAGS "${ARM_FLAGS} -march=armv6")
+elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "^armv7")
+	set(ARM_FLAGS "${ARM_FLAGS} -march=armv7-a")
 else()
-	set(ARM_TARGET_ARCH "${CMAKE_SYSTEM_PROCESSOR}")
+	message("Unknown ARM device detected; if you have troubles, try calling CMake with -DDISABLE_DEFAULT_CFLAGS=1, and set your CFLAGS and CPPFLAGS")
 endif()
 
 # Select the optimal FPU settings
 # If you get linking errors, try editing these flags
-if (ARM_OVERRIDE_FLAGS)
-	set(ARM_FLAGS "${ARM_OVERRIDE_FLAGS}")
-else()
-	set(ARM_FLAGS "-marm")
-	if (ARM_TARGET_ARCH MATCHES "^armv6")
-		set(ARM_FLAGS "${ARM_FLAGS} -march=armv6")
-	elseif (ARM_TARGET_ARCH MATCHES "^armv7")
-		set(ARM_FLAGS "${ARM_FLAGS} -march=armv7-a")
-	else()
-		message("Unknown ARM device detected; if you have troubles, try using ARM_OVERRIDE_FLAGS")
-	endif()
-	set(ARM_FLAGS "${ARM_FLAGS} -mfpu=vfp -mfloat-abi=hard")
-endif()
+set(ARM_FLAGS "${ARM_FLAGS} -mfpu=vfp -mfloat-abi=hard")
 
 # Apply the flags
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${ARM_FLAGS}")
