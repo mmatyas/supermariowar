9ab846ba04edc26653e323cbb8672da072f670b1
AzureBerylBlue <53104010+AzureBerylBlue@users.noreply.github.com>
2019-10-04 16:42:50 -0400

    Tabs to spaces
    Sorry.

--- src/smw/player.cpp
+++ src/smw/player.cpp
@@ -1874,14 +1874,14 @@ short CPlayer::KilledPlayer(CPlayer * killed, short deathstyle, killstyle style,
             killer->SetPowerup(0);
         }
 
-		switch (deathstyle) {
-		case death_style_jump:
-			ifSoundOnPlay(rm->sfx_deathsound);
-		case death_style_squish:
-			ifSoundOnPlay(rm->sfx_mip);
-		case death_style_shatter:
-			ifSoundOnPlay(rm->sfx_breakblock);
-		}
+        switch (deathstyle) {
+        case death_style_jump:
+            ifSoundOnPlay(rm->sfx_deathsound);
+        case death_style_squish:
+            ifSoundOnPlay(rm->sfx_mip);
+        case death_style_shatter:
+            ifSoundOnPlay(rm->sfx_breakblock);
+        }
     }
 
     if (player_kill_normal == iKillType || (fForce && player_kill_nonkill == iKillType))
