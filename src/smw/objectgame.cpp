e5536e67c3bc2b4be05f8fc540ffad8a82d51e87
AzureBerylBlue <53104010+AzureBerylBlue@users.noreply.github.com>
2019-11-16 14:40:03 -0500

    Update objectgame.cpp

--- src/smw/objectgame.cpp
+++ src/smw/objectgame.cpp
@@ -282,10 +282,13 @@ void CheckSecret(short id)
             if (game_values.unlocksecret1part1[iPlayer])
                 iCountTeams++;
         }
+
         if (iCountTeams >= 2 && game_values.unlocksecret1part2 >= 8) {
             game_values.unlocksecretunlocked[0] = true;
             ifSoundOnPlay(rm->sfx_transform);
+
             IO_MovingObject * object = createpowerup(SECRET1_POWERUP, RANDOM_INT(smw->ScreenWidth), RANDOM_INT(smw->ScreenHeight), true, false);
+
             if (object)
                 eyecandy[2].add(new EC_SingleAnimation(&rm->spr_poof, object->ix - 8, object->iy - 8, 4, 5));
         }
@@ -293,7 +296,9 @@ void CheckSecret(short id)
         if (game_values.unlocksecret2part1 && game_values.unlocksecret2part2 >= 3) {
             game_values.unlocksecretunlocked[1] = true;
             ifSoundOnPlay(rm->sfx_transform);
+
             IO_MovingObject * object = createpowerup(SECRET2_POWERUP, RANDOM_INT(smw->ScreenWidth), RANDOM_INT(smw->ScreenHeight), true, false);
+
             if (object)
                 eyecandy[2].add(new EC_SingleAnimation(&rm->spr_poof, object->ix - 8, object->iy - 8, 4, 5));
         }
@@ -303,7 +308,9 @@ void CheckSecret(short id)
             if (game_values.unlocksecret3part1[iPlayer] >= 9 && game_values.unlocksecret3part2[iPlayer] >= 13) {
                 game_values.unlocksecretunlocked[2] = true;
                 ifSoundOnPlay(rm->sfx_transform);
+
                 IO_MovingObject * object = createpowerup(SECRET3_POWERUP, RANDOM_INT(smw->ScreenWidth), RANDOM_INT(smw->ScreenHeight), true, false);
+
                 if (object)
                     eyecandy[2].add(new EC_SingleAnimation(&rm->spr_poof, object->ix - 8, object->iy - 8, 4, 5));
             }
@@ -311,7 +318,9 @@ void CheckSecret(short id)
     } else if (id == 3 && !game_values.unlocksecretunlocked[3]) {
         game_values.unlocksecretunlocked[3] = true;
         ifSoundOnPlay(rm->sfx_transform);
+
         IO_MovingObject * object = createpowerup(SECRET4_POWERUP, RANDOM_INT(smw->ScreenWidth), RANDOM_INT(smw->ScreenHeight), true, false);
+
         if (object)
             eyecandy[2].add(new EC_SingleAnimation(&rm->spr_poof, object->ix - 8, object->iy - 8, 4, 5));
     }
