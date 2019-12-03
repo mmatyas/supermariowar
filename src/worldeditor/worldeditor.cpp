049d8f43ce1138755080e2782bffa8eba2b88b84
AzureBerylBlue <53104010+AzureBerylBlue@users.noreply.github.com>
2019-12-03 17:23:00 -0500

    Update worldeditor.cpp

--- src/worldeditor/worldeditor.cpp
+++ src/worldeditor/worldeditor.cpp
@@ -41,6 +41,10 @@
 #include <string.h>
 #include <ctype.h>
 
+#ifndef USE_SDL2
+#include <algorithm>
+#endif
+
 #ifdef _WIN32
 	#include <windows.h>
     #ifdef _MSC_VER
@@ -102,8 +106,8 @@ WorldMapTile	copiedtiles[MAPWIDTH][MAPHEIGHT];
 int				mouse_x, mouse_y;
 
 void update_mouse_coords() {
-	mouse_x = std::min(640 - 1, std::max(event.button.x, 0));
-	mouse_y = std::min(480 - 1, std::max(event.button.y, 0));
+	mouse_x = std::min(640 - 1, std::max(event.motion.x, 0));
+	mouse_y = std::min(480 - 1, std::max(event.motion.y, 0));
 }
 
 //Vehicle structure that holds the current vehicle "stamp"
