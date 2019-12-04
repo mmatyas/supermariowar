ddfea45a3210c374c86d533cd0cb440aff43f420
AzureBerylBlue <53104010+AzureBerylBlue@users.noreply.github.com>
2019-12-03 19:04:28 -0500

    Update leveleditor.cpp

--- src/leveleditor/leveleditor.cpp
+++ src/leveleditor/leveleditor.cpp
@@ -61,10 +61,6 @@ void removeifprojectile(IO_MovingObject * object, bool playsound, bool forcedead
 #include <cstring>
 #include <cstdlib>
 
-#ifndef USE_SDL2
-#include <algorithm>
-#endif
-
 #ifdef _WIN32
 	#include <windows.h>
     #ifdef _MSC_VER
@@ -171,8 +167,13 @@ short			y_shake = 0;
 int				mouse_x, mouse_y;
 
 void update_mouse_coords() {
+#ifdef USE_SDL2
 	mouse_x = std::min(640 - 1, std::max(event.motion.x, 0));
 	mouse_y = std::min(480 - 1, std::max(event.motion.y, 0));
+#else
+	mouse_x = event.motion.x;
+	mouse_y = event.motion.y;
+#endif
 }
 
 CEyecandyContainer eyecandy[3];
