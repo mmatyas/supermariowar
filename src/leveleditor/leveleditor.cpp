caa68b73e940ae8e7f05fc3d3e766ee2d8fe2e95
AzureBerylBlue <53104010+AzureBerylBlue@users.noreply.github.com>
2019-12-04 16:22:31 -0500

    Update leveleditor.cpp

--- src/leveleditor/leveleditor.cpp
+++ src/leveleditor/leveleditor.cpp
@@ -64,7 +64,6 @@ void removeifprojectile(IO_MovingObject * object, bool playsound, bool forcedead
 #ifdef _WIN32
 	#include <windows.h>
     #ifdef _MSC_VER
-    #define NOMINMAX
     #if _MSC_VER >= 1400
         #include <stdio.h>
         FILE _iob[] = {*stdin, *stdout, *stderr};
@@ -169,8 +168,8 @@ int				mouse_x, mouse_y;
 
 void update_mouse_coords() {
 #ifdef USE_SDL2
-	mouse_x = std::min(640 - 1, std::max(event.motion.x, 0));
-	mouse_y = std::min(480 - 1, std::max(event.motion.y, 0));
+	mouse_x = (std::min)(640 - 1, (std::max)(event.motion.x, 0));
+	mouse_y = (std::min)(480 - 1, (std::max)(event.motion.y, 0));
 #else
 	mouse_x = event.motion.x;
 	mouse_y = event.motion.y;
