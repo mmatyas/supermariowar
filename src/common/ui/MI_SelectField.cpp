e17c20c8e6d5fb3e692f1ac7e8ea2fc1f7e9902d
Mátyás Mustoha <matyas.mustoha@gmail.com>
2024-03-30 18:41:06 +0100

    Implemented a templated MI_SelectField alternative

--- src/common/ui/MI_SelectField.cpp
+++ src/common/ui/MI_SelectField.cpp
@@ -481,3 +481,320 @@ void MI_SelectField::Refresh()
     else if (fValue)
         SetKey(*fValue ? 1 : 0);
 }
+
+
+template<typename T>
+MI_SelectFieldDyn<T>::MI_SelectFieldDyn(gfxSprite* nspr, short x, short y, std::string name, short width, short indent)
+    : UI_Control(x, y)
+    , m_spr(nspr)
+    , m_name(std::move(name))
+    , m_adjustmentY(width > 256 ? 0 : 128)
+    , m_width(width)
+    , m_indent(indent)
+{
+    miModifyImageLeft = std::make_unique<MI_Image>(nspr, ix + indent - 26, iy + 4, 32, 64, 26, 24, 4, 1, 8);
+    miModifyImageLeft->Show(false);
+
+    miModifyImageRight = std::make_unique<MI_Image>(nspr, ix + width - 16, iy + 4, 32, 88, 26, 24, 4, 1, 8);
+    miModifyImageRight->Show(false);
+}
+
+
+template<typename T>
+void MI_SelectFieldDyn<T>::Update()
+{
+    miModifyImageRight->Update();
+    miModifyImageLeft->Update();
+}
+
+
+template<typename T>
+void MI_SelectFieldDyn<T>::Draw()
+{
+    if (!fShow)
+        return;
+
+    if (m_indent == 0) {
+        short iHalfWidth = m_width / 2;
+        m_spr->draw(ix, iy, 0, (fSelected ? 32 : 0) + m_adjustmentY, iHalfWidth, 32);
+        m_spr->draw(ix + iHalfWidth, iy, 512 - iHalfWidth, (fSelected ? 32 : 0) + m_adjustmentY, m_width - iHalfWidth, 32);
+    } else {
+        m_spr->draw(ix, iy, 0, (fSelected ? 32 : 0) + m_adjustmentY, m_indent - 16, 32);
+        m_spr->draw(ix + m_indent - 16, iy, 0, (fSelected ? 96 : 64), 32, 32);
+        m_spr->draw(ix + m_indent + 16, iy, 528 - m_width + m_indent, (fSelected ? 32 : 0) + m_adjustmentY, m_width - m_indent - 16, 32);
+    }
+
+    if (m_indent> 0)
+        rm->menu_font_large.drawChopRight(ix + 16, iy + 5, m_indent - 8, m_name.c_str());
+
+    if (!m_items.empty()) {
+        const short indent = (m_indent > 0) ? m_indent : 8;
+        rm->menu_font_large.drawChopRight(ix + indent + 8, iy + 5, m_width - indent - 24, currentItem().name.c_str());
+    }
+
+    const bool drawLeft = m_index > 0;
+    if (m_wraps || drawLeft)
+        miModifyImageLeft->Draw();
+
+    const bool drawRight = (m_index + 1) < m_items.size();
+    if (m_wraps || drawRight)
+        miModifyImageRight->Draw();
+}
+
+
+template<typename T>
+MenuCodeEnum MI_SelectFieldDyn<T>::Modify(bool modify)
+{
+    if (fDisable)
+        return MENU_CODE_UNSELECT_ITEM;
+
+    if (m_autoAdvance && modify) {
+        moveNext();
+        return mcItemChangedCode;
+    }
+
+    if (MENU_CODE_NONE != mcControlSelectedCode)
+        return mcControlSelectedCode;
+
+    miModifyImageLeft->Show(modify);
+    miModifyImageRight->Show(modify);
+    fModifying = modify;
+    return MENU_CODE_MODIFY_ACCEPTED;
+}
+
+
+template<typename T>
+MenuCodeEnum MI_SelectFieldDyn<T>::SendInput(CPlayerInput * playerInput)
+{
+    for (int iPlayer = 0; iPlayer < 4; iPlayer++) {
+        short iNumMoves = 1;
+        if (m_fastScroll && playerInput->outputControls[iPlayer].menu_scrollfast.fDown)
+            iNumMoves = 10;
+
+        if (playerInput->outputControls[iPlayer].menu_right.fPressed || playerInput->outputControls[iPlayer].menu_down.fPressed) {
+            bool fMoved = false;
+
+            for (short iMove = 0; iMove < iNumMoves; iMove++)
+                fMoved |= moveNext();
+
+            if (fMoved)
+                return mcItemChangedCode;
+        }
+
+        if (playerInput->outputControls[iPlayer].menu_left.fPressed || playerInput->outputControls[iPlayer].menu_up.fPressed) {
+            bool fMoved = false;
+
+            for (short iMove = 0; iMove < iNumMoves; iMove++)
+                fMoved |= movePrev();
+
+            if (fMoved)
+                return mcItemChangedCode;
+        }
+
+        if (playerInput->outputControls[iPlayer].menu_random.fPressed) {
+            if (moveRandom())
+                return mcItemChangedCode;
+        }
+
+        if (playerInput->outputControls[iPlayer].menu_select.fPressed || playerInput->outputControls[iPlayer].menu_cancel.fPressed) {
+            miModifyImageLeft->Show(false);
+            miModifyImageRight->Show(false);
+
+            fModifying = false;
+
+            updateOutput();
+
+            return MENU_CODE_UNSELECT_ITEM;
+        }
+    }
+
+    return MENU_CODE_NONE;
+}
+
+
+template<typename T>
+MenuCodeEnum MI_SelectFieldDyn<T>::MouseClick(short iMouseX, short iMouseY) {
+    if (fDisable)
+        return MENU_CODE_NONE;
+
+    //If we are modifying this control, see if we clicked on a next/prev button
+    if (fModifying) {
+        short x, y, w, h;
+
+        miModifyImageLeft->GetPositionAndSize(x, y, w, h);
+        if (x <= iMouseX && iMouseX < x + w && y <= iMouseY && iMouseY < y + h) {
+            if (movePrev()) {
+                return mcItemChangedCode == MENU_CODE_NONE
+                    ? MENU_CODE_CLICKED
+                    : mcItemChangedCode;
+            }
+        }
+
+        miModifyImageRight->GetPositionAndSize(x, y, w, h);
+        if (x <= iMouseX && iMouseX < x + w && y <= iMouseY && iMouseY < y + h) {
+            if (moveNext()) {
+                return mcItemChangedCode == MENU_CODE_NONE
+                    ? MENU_CODE_CLICKED
+                    : mcItemChangedCode;
+            }
+        }
+    }
+
+    //Otherwise just check to see if we clicked on the whole control
+    if (ix <= iMouseX && iMouseX < ix + m_width && iy <= iMouseY && iMouseY < iy + 32)
+        return MENU_CODE_CLICKED;
+
+    //Otherwise this control wasn't clicked at all
+    return MENU_CODE_NONE;
+}
+
+
+template<typename T>
+void MI_SelectFieldDyn<T>::Refresh()
+{
+    if (m_outputPtr)
+        setCurrentValue(*m_outputPtr);
+}
+
+
+template<typename T>
+bool MI_SelectFieldDyn<T>::setCurrentValue(T value)
+{
+    for (m_index = 0; m_index < m_items.size(); m_index++) {
+        if (m_items[m_index].value == value) {
+            updateOutput();
+            return true;
+        }
+    }
+    return false;
+}
+
+
+template<typename T>
+bool MI_SelectFieldDyn<T>::setCurrentIndex(size_t index)
+{
+    if (m_index < m_items.size()) {
+        m_index = index;
+        updateOutput();
+        return true;
+    }
+    return false;
+}
+
+
+template<typename T>
+void MI_SelectFieldDyn<T>::updateOutput() const
+{
+    if (m_outputPtr)
+        *m_outputPtr = currentValue();
+}
+
+
+template<typename T>
+void MI_SelectFieldDyn<T>::add(std::string name, T value, bool hidden, bool goodRandom, short iconOverride)
+{
+    size_t new_idx = m_items.size();
+    m_items.emplace_back(std::move(name), std::move(value), hidden, iconOverride);
+    m_index = 0;
+
+    if (goodRandom)
+        m_goodRandomIndices.push_back(new_idx);
+}
+
+
+template<typename T>
+void MI_SelectFieldDyn<T>::clear()
+{
+    m_items.clear();
+    m_goodRandomIndices.clear();
+}
+
+
+template<typename T>
+void MI_SelectFieldDyn<T>::hideItem(T value, bool hide)
+{
+    for (Item& item : m_items) {
+        if (item.value == value)
+            item.hidden = hide;
+    }
+    if (currentItem().hidden)
+        moveNext() || movePrev();
+}
+
+
+template<typename T>
+void MI_SelectFieldDyn<T>::hideAllItems(bool hide)
+{
+    for (Item& item : m_items) {
+        item.hidden = hide;
+    }
+}
+
+
+template<typename T>
+bool MI_SelectFieldDyn<T>::moveNext()
+{
+    if (m_items.empty())
+        return false;
+
+    // stop when detecting a restart or a loop
+    const size_t stop_idx = m_wraps ? m_index : 0;
+
+    while (true) {
+        m_index = (m_index + 1) % m_items.size();
+        if (m_index == stop_idx)
+            break;
+        if (!currentItem().hidden) {
+            updateOutput();
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+template<typename T>
+bool MI_SelectFieldDyn<T>::movePrev()
+{
+    if (m_items.empty())
+        return false;
+
+    // stop when detecting a restart or a loop
+    const size_t stop_idx = m_wraps ? m_index : (m_items.size() - 1);
+
+    while (true) {
+        m_index = (m_index + m_items.size() - 1) % m_items.size();
+        if (m_index == stop_idx)
+            break;
+        if (!currentItem().hidden) {
+            updateOutput();
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+template<typename T>
+bool MI_SelectFieldDyn<T>::moveRandom()
+{
+    std::vector<size_t> valid_indices;
+    valid_indices.reserve(m_items.size());
+
+    for (size_t idx = 0; idx < m_items.size(); idx++) {
+        if (!m_items[idx].hidden && idx != m_index)
+            valid_indices.push_back(idx);
+    }
+    if (valid_indices.size() <= 1)
+        return false;
+
+    m_index = valid_indices[RANDOM_INT(valid_indices.size())];
+    updateOutput();
+    return true;
+}
+
+
+// Actual typed classes
+template class MI_SelectFieldDyn<bool>;
--- src/common/ui/MI_SelectField.h
+++ src/common/ui/MI_SelectField.h
@@ -7,6 +7,7 @@
 
 #include <memory>
 #include <string>
+#include <type_traits>
 #include <vector>
 
 
@@ -185,5 +186,129 @@ protected:
 
     bool fFastScroll = false;
 };
+
+
+template<typename T>
+struct SF_ListItemDyn {
+    SF_ListItemDyn(std::string&& name, T value, bool hidden, short iconOverride)
+        : name(std::move(name))
+        , value(value)
+        , hidden(hidden)
+        , iconOverride(iconOverride)
+    {
+        static_assert(std::is_trivially_copyable<T>::value, "");
+    }
+
+    /*SF_ListItemDyn(const SF_ListItemDyn& other)
+        : name(other.name)
+        , value(other.value)
+        , hidden(other.hidden)
+        , iconOverride(other.iconOverride)
+    {}*/
+
+    const std::string name;  //!< Display name
+    const T value;
+    bool hidden = false;
+    short iconOverride = -1;
+};
+
+
+template<typename T>
+class MI_SelectFieldDyn : public UI_Control {
+    using Item = SF_ListItemDyn<T>;
+
+public:
+    MI_SelectFieldDyn(gfxSprite* nspr, short x, short y, std::string name, short width, short indent);
+    virtual ~MI_SelectFieldDyn() = default;
+
+    //! Set the displayed name.
+    void setTitle(std::string name) {
+        m_name = std::move(name);
+    }
+    //! When this control is selected, advance the value one instead of modifying the control.
+    void setAutoAdvance(bool advance) {
+        m_autoAdvance = advance;
+    }
+    void allowWrap(bool wraps) {
+        m_wraps = wraps;
+    }
+    //When the item is changed, this code will be returned from SendInput()
+    void setItemChangedCode(MenuCodeEnum code) {
+        mcItemChangedCode = code;
+    }
+    void setControlSelectedCode(MenuCodeEnum code) {
+        mcControlSelectedCode = code;
+    }
+    void allowFastScroll(bool fastscroll) {
+        m_fastScroll = fastscroll;
+    }
+    //! Set where the data of this control is written to (some member of game_values probably)
+    void setOutputPtr(T* ptr) {
+        m_outputPtr = ptr;
+    }
+
+    //! Gets the currently selected item
+    const Item& currentItem() const {
+        return m_items.at(m_index);
+    }
+    //! Gets the currently selected item's value
+    T currentValue() const {
+        return currentItem().value;
+    }
+    //! Gets a random value, but does not set the control to that value
+    T randomValue() const {
+        const size_t idx = m_goodRandomIndices.at(RANDOM_INT(m_goodRandomIndices.size()));
+        return m_items.at(idx).value;
+    }
+
+    //! Set the current selected item based on its value
+    bool setCurrentValue(T value);
+    //! Sets the currently selected item
+    bool setCurrentIndex(size_t index);
+    //! Adds an item to the list
+    void add(std::string name, T value, bool hidden = false, bool goodRandom = true, short iconOverride = -1);
+    //! Removes all items
+    void clear();
+    //! Hides the items containing the specified value
+    void hideItem(T value, bool hide);
+    //! Hides all items
+    void hideAllItems(bool hide);
+
+public:
+    void Update() override;  //! Updates animations or other events every frame
+    void Draw() override;  //! Draws every frame
+    void Refresh() override;
+    MenuCodeEnum Modify(bool modify) override;  //! Called when user selects this control to change it's value
+    MenuCodeEnum SendInput(CPlayerInput* playerInput) override;  //! Sends player input to control on every frame
+    MenuCodeEnum MouseClick(short iMouseX, short iMouseY) override;
+
+protected:
+    gfxSprite* m_spr = nullptr;
+    std::string m_name;
+
+    std::unique_ptr<MI_Image> miModifyImageLeft;
+    std::unique_ptr<MI_Image> miModifyImageRight;
+
+    MenuCodeEnum mcItemChangedCode = MENU_CODE_NONE;
+    MenuCodeEnum mcControlSelectedCode = MENU_CODE_NONE;
+
+    bool m_autoAdvance = false;
+    bool m_wraps = true;
+    bool m_fastScroll = false;
+    short m_adjustmentY = 0;
+    short m_width = 0;
+    short m_indent = 0;
+
+    std::vector<Item> m_items;
+    std::vector<size_t> m_goodRandomIndices;
+    size_t m_index = 0;
+    T* m_outputPtr = nullptr;
+
+    void updateOutput() const;
+    bool moveNext();
+    bool movePrev();
+    bool moveRandom();
+};
+
 
 #endif // UI_SELECT_FIELD_H
