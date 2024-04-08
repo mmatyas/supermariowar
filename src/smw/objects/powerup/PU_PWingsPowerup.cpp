4cba698d543c685f09f2adef43d3d3bb971260f6
Mátyás Mustoha <matyas.mustoha@gmail.com>
2024-04-08 15:54:01 +0200

    Fixed a minor style issue

--- src/smw/objects/powerup/PU_PWingsPowerup.cpp
+++ src/smw/objects/powerup/PU_PWingsPowerup.cpp
@@ -7,8 +7,7 @@
 //------------------------------------------------------------------------------
 PU_PWingsPowerup::PU_PWingsPowerup(gfxSprite* nspr, short x, short y)
     : MO_Powerup(nspr, x, y, 1, 0, 30, 30, 1, 1)
-{
-}
+{}
 
 bool PU_PWingsPowerup::collide(CPlayer* player)
 {
