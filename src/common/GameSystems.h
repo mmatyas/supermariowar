9eb9cd4f26b18adb3dadb4bf656e10dcf0487f01
Mátyás Mustoha <matyas.mustoha@gmail.com>
2026-03-19 18:25:33 +0100

    Moved SDL initialization to its own object

--- src/common/GameSystems.h
+++ src/common/GameSystems.h
@@ -3,10 +3,20 @@
 #include "gfx/gfxSDL.h"
 
 
+struct Sdl {
+    Sdl();
+    ~Sdl();
+};
+
+
 struct Systems {
     Systems(bool fullscreen)
         : gfx(fullscreen)
     {}
 
+    Systems(const Systems&) = delete;
+    Systems& operator=(const Systems&) = delete;
+
+    Sdl sdl;
     Graphics gfx;
 };
